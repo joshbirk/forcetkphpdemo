99cdf63f10ba87c75c8c1dcc780703ea875c1810
joshbirk <joshua.birk@salesforce.com>
2013-04-03 12:27:28 -0500

    various fixes

--- index.php
+++ index.php
@@ -61,19 +61,21 @@ if (window.$j === undefined) {
 }
 
 $j(document).ready(function() {
-	var oauthResponse = {};
+	if(client.sessionId == null) {
+		var oauthResponse = {};
 
-	if (window.location.hash) {
-		var message = window.location.hash.substr(1);
-		var nvps = message.split('&');
-		for (var nvp in nvps) {
-		    var parts = nvps[nvp].split('=');
-			oauthResponse[parts[0]] = unescape(parts[1]);
-			sessionCallback(oauthResponse);
+		if (window.location.hash) {
+			var message = window.location.hash.substr(1);
+			var nvps = message.split('&');
+			for (var nvp in nvps) {
+			    var parts = nvps[nvp].split('=');
+				oauthResponse[parts[0]] = unescape(parts[1]);
+				sessionCallback(oauthResponse);
+			}
+		} else {
+			url = getAuthorizeUrl(loginUrl, clientId, redirectUri);
+			window.location.href = url;
 		}
-	} else {
-		url = getAuthorizeUrl(loginUrl, clientId, redirectUri);
-		window.location.href = url;
 	}
 });
 
